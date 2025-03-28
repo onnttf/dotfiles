52fc5d253e1845790b0036f3b0102fce88c8594c
Zhang Peng <onnttf@gmail.com>
2025-03-28 09:05:31 +0800

    refactor(neovim): refactor configuration into modular structure

---
+++ nvim/lua/util.lua
@@ -0,0 +1,13 @@
+-- Default options for key mappings
+local default_opts = { noremap = true, silent = true }
+
+-- Helper function to create key mappings with default options merged
+local function keymap(mode, lhs, rhs, opts)
+    opts = opts or {}
+    opts = vim.tbl_extend("force", default_opts, opts)
+    vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+return {
+    keymap = keymap
+}
