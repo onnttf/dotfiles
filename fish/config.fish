53d519e84dae048b00aefca4c6b4954f55e95961
Zhang Peng <onnttf@gmail.com>
2025-05-29 00:47:56 +0800

    refactor(fish): add MySQL bin path

--- fish/config.fish
+++ fish/config.fish
@@ -1,38 +1,41 @@
 # Disable the default Fish greeting message
 set -U fish_greeting ""
 
-# Check if Homebrew is installed, and if so, initialize its environment
+# Initialize Homebrew if installed
 if test -x /opt/homebrew/bin/brew
     eval (/opt/homebrew/bin/brew shellenv)
 end
 
-# Add Go binaries to PATH if Go is installed
+# Add Go bin path if Go is installed
 if command -q go
-    # Use `go env GOPATH` to get the GOPATH and add its bin directory to PATH
     fish_add_path (go env GOPATH)/bin
 end
 
-# Define an alias 'ip' to display IPv4 address information
-# Uses ifconfig to get network interface details, and awk to filter for IPv4 addresses and interface names
+# Add MySQL 8.4 bin path if installed
+if test -d /opt/homebrew/opt/mysql@8.4/bin
+    fish_add_path /opt/homebrew/opt/mysql@8.4/bin
+end
+
+# Show IPv4 addresses
 alias ip="ifconfig | awk '/inet / && !/inet6/ {print \$2, \$1}'"
 
-# Define an alias 'h' for quickly navigating to the user's home directory
+# Go to home directory
 alias h="cd \"$HOME\""
 
-# Define a function 'now' to display the current date and time and Unix timestamp
+# Show current date, time, and timestamp
 function now
-    echo -n "Current Date and Time: "
+    echo -n "Datetime: "
     date '+%Y-%m-%dT%H:%M:%S%z'
-    echo -n "Unix Timestamp: "
+    echo -n "Timestamp: "
     date +%s
 end
 
-# Define a function 'timestamp' to display the current Unix timestamp
+# Show current timestamp
 function timestamp
     date +%s
 end
 
-# Define a function 'format_timestamp' to format a given Unix timestamp into a human-readable date and time
+# Format a given timestamp
 # Usage: format_timestamp <timestamp>
 function format_timestamp
     date -r $argv[1] "+%Y-%m-%d %H:%M:%S"
