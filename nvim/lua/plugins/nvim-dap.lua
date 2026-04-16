1b1891a15478079952e34c2580a4a547037663e3
ZHANG PENG <onnttf@gmail.com>
2026-04-16 20:00:00 +0800

    feat(neovim): add DAP debugging support

---
+++ nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,34 @@
+-- https://github.com/mfussenegger/nvim-dap
+return {
+	"mfussenegger/nvim-dap",
+	cmd = {
+		"DapContinue",
+		"DapToggleBreakpoint",
+		"DapStepOver",
+		"DapStepInto",
+		"DapStepOut",
+		"DapTerminate",
+	},
+	dependencies = {
+		{ "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" } },
+		{ "theHamsta/nvim-dap-virtual-text" },
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		dapui.setup()
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+		require("nvim-dap-virtual-text").setup()
+	end,
+}
